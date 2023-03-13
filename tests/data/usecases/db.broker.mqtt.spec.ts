ee7333fd11dd31848cc425f6d7ddcaf166bc638f
guiac <gui.acassemiro@gmail.com>
2023-03-12 21:00:57 -0300

    :test_tube: DbMqttBroker

---
+++ tests/data/usecases/db.broker.mqtt.spec.ts
@@ -0,0 +1,43 @@
+import { DbMqttBroker } from '@/data/usecases'
+import { MqttBroker } from '@/domain/usecases'
+
+type SutTypes = {
+    sut: MqttBroker
+}
+
+const throwError = (): never => {
+    throw new Error()
+}
+
+const makeSut = (): SutTypes => {
+    const sut = new DbMqttBroker()
+    return { sut }
+}
+
+const mockRequest = (): MqttBroker.Params => ({
+    deviceIdentification: 'deviceIdentification',
+    actuatorIdentification: 'actuatorIdentification',
+    actuatorCurrentValue: 0
+})
+
+const MqttClient = {
+    publish() {
+        return true
+    }
+}
+
+describe('DbMqttBroker', () => {
+    test('Should return true if MqttClient is valid', async () => {
+        const { sut } = makeSut()
+        const request = mockRequest()
+        const resp = await sut.handle(request, MqttClient)
+        expect(resp).toBeTruthy()
+    })
+
+    test('Should return false if MqttClient is empty', async () => {
+        const { sut } = makeSut()
+        const request = mockRequest()
+        const resp = await sut.handle(request, false)
+        expect(resp).toBeFalsy()
+    })
+})
